122393d468333bbdcf618bca048ace90331ec79a
Usman Qasim <usmanaa552@gmail.com>
2024-12-24 14:39:22 +0500

    to do app

--- TO DO LIST/todo.js
+++ TO DO LIST/todo.js
@@ -1,28 +1,39 @@
-const input=document.getElementById('field');
-const btn=document.getElementById('add');
-const listitem=document.getElementById('listitem');
+const input = document.getElementById('field');
+const btn = document.getElementById('add');
+const listitem = document.getElementById('listitem');
 
-btn.addEventListener("click",function(){
-    if(input.value===''){
+btn.addEventListener("click", function () {
+    if (input.value === '') {
         alert("Must fill the input field");
-        savedata()
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = input.value;
+        listitem.appendChild(li);
+        input.value = "";
+        
+        li.addEventListener("click", function () {
+            listitem.removeChild(li);
+            savedata();
+        });
+
+        savedata(); 
     }
-    else{
-    let li=document.createElement("li");
-    li.innerHTML=input.value;
-    listitem.appendChild(li);
-    input.value="";
-    li.addEventListener("click",function(){
-        listitem.removeChild(li);
-    })
-    savedata()
-    }
-    
 });
-function savedata(){
-    localStorage.setItem("data",listitem.innerHTML);
+
+function savedata() {
+    localStorage.setItem("data", listitem.innerHTML);
 }
-function showitem(){
-    listitem.innerHTML=localStorage.getItem("data");
+
+function showitem() {
+    listitem.innerHTML = localStorage.getItem("data");
+    const allItems = listitem.querySelectorAll("li");
+    allItems.forEach(item => {
+    
+        item.addEventListener("click", function () {
+            listitem.removeChild(item);
+            savedata();
+        });
+    });
 }
+
 showitem();
