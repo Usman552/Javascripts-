7bd100b64755fa701a781f416373a4dd4f847ab9
Usman Qasim <usmanaa552@gmail.com>
2025-03-03 11:42:46 +0500

    sent friend request

--- friend-request/script.js
+++ friend-request/script.js
@@ -8,13 +8,15 @@ btn.addEventListener("click",()=>{
     if(check==0){
         istatus.innerHTML = "Friends";
         istatus.style.color = "green";
-        btn.innerHTML = "Unfriend";
+        btn.innerHTML = "Remove friend";
+        btn.style.backgroundColor = "grey";
         check=1;
     }
     else{
         istatus.innerHTML = "RSO";
         istatus.style.color = "red";
         btn.innerHTML = "Add Friend";
+        btn.style.backgroundColor = "green";
         check=0;
     }
 
