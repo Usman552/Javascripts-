3f5fa151d341a49fa2c995633365d1f991360a31
Usman Qasim <usmanaa552@gmail.com>
2025-03-03 12:55:16 +0500

    intagram like love icon

--- Like-Love-icon/script.js
+++ Like-Love-icon/script.js
@@ -1,9 +1,23 @@
 let box=document.querySelector(".box");
 let love=document.getElementById("icon");
-console.log(love)
+let heart=document.getElementById("love");
+
 box.addEventListener("dblclick",()=>{
     love.style.transform='translate(-50%, -50%)scale(1)';
     setTimeout(()=>{
         love.style.transform='translate(-50%, -50%)scale(0)';
     },1000)
 })
+let check=0;
+heart.addEventListener("click",()=>{
+    if(check==0){
+        heart.className="ri-heart-3-fill";
+    heart.style.color="red";
+        check=1;
+    }
+    else{
+        heart.className="ri-heart-3-line";
+        heart.style.color="black";
+        check=0;
+    }
+});
